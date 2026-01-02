f6ce4d2d72cfcb81c344e5975aa7d4a7e887a335
Beat Hagenlocher <bah@posteo.de>
2026-01-02 19:19:56 +0100

    Add caddy routes & backup

--- clan.nix
+++ clan.nix
@@ -7,7 +7,7 @@
     # Define machines here.
     formenos = {
       deploy.targetHost = "root@49.12.12.164";
-      tags = [  ];
+      tags = [ ];
     };
   };
 
@@ -23,7 +23,8 @@
         # Insert the public key that you want to use for SSH access.
         # All keys will have ssh access to all machines ("tags.all" means 'all machines').
         # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
-        "beat" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILD4jhHznn3K7uZGhNTu3En3vDyiLmbColfok2Qm/MKS beat@gondor";
+        "beat" =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILD4jhHznn3K7uZGhNTu3En3vDyiLmbColfok2Qm/MKS beat@gondor";
       };
     };
 
@@ -67,66 +68,87 @@
   # machines/jon/configuration.nix will be automatically imported.
   # See: https://docs.clan.lol/guides/more-machines/#automatic-registration
   machines = {
-    formenos = { _config, pkgs, ... }: {
-      imports = [
-        ./modules/logto-compose.nix
-        ./modules/logto-postgres-login.nix
-      ];
-      environment.systemPackages = [ pkgs.git ];
-
-      networking.firewall.allowedTCPPorts = [ 80 443 ];
-
-      services.caddy = {
-        enable = true;
-        virtualHosts."paperless.hagenlocher.me" = {
-          extraConfig = ''
-            reverse_proxy 127.0.0.1:28981
-          '';
-        };
-        virtualHosts."finances.hagenlocher.me" = {
-          extraConfig = ''
-            reverse_proxy 127.0.0.1:5006
-          '';
-        };
-      };
-
-      # https://docs.clan.lol/guides/backups/backup-intro/
-      clan.core.state.actual = {
-        folders = [
-          "/var/lib/actual"
+    formenos =
+      { _config, pkgs, ... }:
+      {
+        imports = [
+          ./modules/logto-compose.nix
+          ./modules/logto-postgres-login.nix
         ];
-      };
-      # https://search.nixos.org/options?channel=unstable&query=services.actual
-      services.actual = {
-        enable = true;
-        settings = {
-          port = 5006;
-        };
-      };
+        environment.systemPackages = [ pkgs.git ];
 
-      # https://docs.clan.lol/guides/backups/backup-intro/
-      clan.core.state.paperless = {
-        folders = [
-          "/var/lib/paperless"
+        networking.firewall.allowedTCPPorts = [
+          80
+          443
         ];
-      };
-      # https://wiki.nixos.org/wiki/Paperless-ngx
-      services.paperless = {
-        enable = true;
-        consumptionDirIsPublic = true;
-        settings = {
-          PAPERLESS_CONSUMER_IGNORE_PATTERN = [
-            ".DS_STORE/*"
-            "desktop.ini"
-          ];
-          PAPERLESS_OCR_LANGUAGE = "deu+eng";
-          PAPERLESS_OCR_USER_ARGS = {
-            optimize = 1;
-            pdfa_image_compression = "lossless";
+
+        services.caddy = {
+          enable = true;
+          virtualHosts."paperless.hagenlocher.me" = {
+            extraConfig = ''
+              reverse_proxy 127.0.0.1:28981
+            '';
           };
-          PAPERLESS_URL = "https://paperless.hagenlocher.me";
+          virtualHosts."finances.hagenlocher.me" = {
+            extraConfig = ''
+              reverse_proxy 127.0.0.1:5006
+            '';
+          };
+          virtualHosts."auth-todos.hagenlocher.me" = {
+            extraConfig = ''
+              reverse_proxy 127.0.0.1:3001
+            '';
+          };
+          virtualHosts."admin-todos.hagenlocher.me" = {
+            extraConfig = ''
+              reverse_proxy 127.0.0.1:3002
+            '';
+          };
+        };
+
+        clan.core.state.todo-home = {
+          folders = [
+            "/var/lib/containers/volumes/logto_postgres_data"
+          ];
+        };
+
+        # https://docs.clan.lol/guides/backups/backup-intro/
+        clan.core.state.actual = {
+          folders = [
+            "/var/lib/actual"
+          ];
+        };
+        # https://search.nixos.org/options?channel=unstable&query=services.actual
+        services.actual = {
+          enable = true;
+          settings = {
+            port = 5006;
+          };
+        };
+
+        # https://docs.clan.lol/guides/backups/backup-intro/
+        clan.core.state.paperless = {
+          folders = [
+            "/var/lib/paperless"
+          ];
+        };
+        # https://wiki.nixos.org/wiki/Paperless-ngx
+        services.paperless = {
+          enable = true;
+          consumptionDirIsPublic = true;
+          settings = {
+            PAPERLESS_CONSUMER_IGNORE_PATTERN = [
+              ".DS_STORE/*"
+              "desktop.ini"
+            ];
+            PAPERLESS_OCR_LANGUAGE = "deu+eng";
+            PAPERLESS_OCR_USER_ARGS = {
+              optimize = 1;
+              pdfa_image_compression = "lossless";
+            };
+            PAPERLESS_URL = "https://paperless.hagenlocher.me";
+          };
         };
       };
-    };
   };
 }
