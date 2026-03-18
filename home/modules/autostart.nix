2d8ae1b6daf5d3691c705b029625346508aa9c5e
Beat Hagenlocher <bah@posteo.de>
2026-03-18 14:22:10 +0100

    Adjust autostart/shortcuts for mycelium emacs

--- home/modules/autostart.nix
+++ home/modules/autostart.nix
@@ -15,17 +15,28 @@
       X-GNOME-Autostart-enabled=true
       OnlyShowIn=GNOME;
     '';
+    ".config/autostart/emacs-mycelium.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Emacs Mycelium
+      Comment=Start a separate Emacs instance sporting mycelium (and other docs stuff)
+      Exec=${pkgs.emacs}/bin/emacs --name "mycelium"
+      X-GNOME-Autostart-enabled=true
+      OnlyShowIn=GNOME;
+    '';
 
     ".config/autostart/linphone.desktop".source = "${
       inputs.nixpkgs-24-11.legacyPackages."x86_64-linux".linphone
     }/share/applications/linphone.desktop";
     ".config/autostart/firefox.desktop".source = "${pkgs.firefox}/share/applications/firefox.desktop";
-    ".config/autostart/thunderbird.desktop".source = "${pkgs.thunderbird}/share/applications/thunderbird.desktop";
+    ".config/autostart/thunderbird.desktop".source =
+      "${pkgs.thunderbird}/share/applications/thunderbird.desktop";
     # ".config/autostart/librewolf.desktop".source =
     #   "${pkgs.librewolf}/share/applications/librewolf.desktop";
     ".config/autostart/chromium.desktop".source =
       "${pkgs.chromium}/share/applications/chromium-browser.desktop";
     ".config/autostart/emacs.desktop".source = "${pkgs.emacs}/share/applications/emacs.desktop";
-    ".config/autostart/obsidian.desktop".source = "${pkgs.obsidian}/share/applications/obsidian.desktop";
+    ".config/autostart/obsidian.desktop".source =
+      "${pkgs.obsidian}/share/applications/obsidian.desktop";
   };
 }
