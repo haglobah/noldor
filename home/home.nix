76aaef2cc8b0eb726690f82f39ba14990d9f9247
Beat Hagenlocher <bah@posteo.de>
2026-05-18 23:05:23 +0200

    Pull out kitty

--- home/home.nix
+++ home/home.nix
@@ -20,6 +20,7 @@
     ./secrets.nix
 
     ./programs/git.nix
+    ./programs/kitty.nix
     # ./programs/vscode.nix
     # ./programs/nvf.nix
     ./programs/bash.nix
@@ -320,47 +321,6 @@
           # ctrl+g
           # "ctrl+plus=increase_font_size:1"
         ];
-      };
-    };
-
-    programs.kitty = {
-      enable = true;
-      shellIntegration.enableBashIntegration = true;
-      shellIntegration.enableFishIntegration = true;
-
-      settings = {
-        enabled_layouts = "splits:split_axis=horizontal";
-        allow_remote_control = "yes";
-        hide_window_decorations = "yes";
-        font_size = 16;
-        # This is for enabling a global visual bell. However, this doesn't seem to work (only makes the screen brighter, not less bright again)
-        # enable_audio_bell = "no";
-        # visual_bell_duration = 0;
-        # command_on_bell = "${pkgs.brightnessctl}/bin/brightnessctl s +10%; sleep 0.05; ${pkgs.brightnessctl}/bin/brightnessctl s 10%-";
-      };
-
-      keybindings = {
-        "ctrl+t" = "launch --cwd=current --type=tab";
-        "ctrl+alt+c" = "launch --cwd=current --type=tab claude --model opus";
-        "alt+left" = "prev_tab";
-        "alt+right" = "next_tab";
-        "alt+shift+left" = "move_tab_backward";
-        "alt+shift+right" = "next_tab_forward";
-        "ctrl+]" = "launch --cwd=current --location=vsplit";
-        "ctrl+[" = "launch --cwd=current --location=hsplit";
-        "shift+left" = "neighboring_window left";
-        "shift+right" = "neighboring_window right";
-        "shift+up" = "neighboring_window up";
-        "shift+down" = "neighboring_window down";
-        "ctrl+shift+left" = "move_window left";
-        "ctrl+shift+right" = "move_window right";
-        "ctrl+shift+up" = "move_window up";
-        "ctrl+shift+down" = "move_window down";
-        "ctrl+plus" = "change_font_size all +1.0";
-        "ctrl+equal" = "change_font_size all 10.0";
-        "ctrl+minus" = "change_font_size all -1.0";
-        "ctrl+s" = "send_text all \\x17";
-        "ctrl+g" = "send_key alt+d";
       };
     };
 
---
+++ home/programs/kitty.nix
@@ -0,0 +1,63 @@
+{ ... }:
+{
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableBashIntegration = true;
+    shellIntegration.enableFishIntegration = true;
+
+    settings = {
+      enabled_layouts = "splits:split_axis=horizontal";
+      allow_remote_control = "yes";
+      hide_window_decorations = "yes";
+      font_size = 16;
+      # This is for enabling a global visual bell. However, this doesn't seem to work (only makes the screen brighter, not less bright again)
+      # enable_audio_bell = "no";
+      # visual_bell_duration = 0;
+      # command_on_bell = "${pkgs.brightnessctl}/bin/brightnessctl s +10%; sleep 0.05; ${pkgs.brightnessctl}/bin/brightnessctl s 10%-";
+    };
+
+    keybindings = {
+      "ctrl+shift+space" = "push_keyboard_mode scroll";
+      "ctrl+t" = "launch --cwd=current --type=tab";
+      "ctrl+alt+c" = "launch --cwd=current --type=tab claude --model opus";
+      "alt+left" = "prev_tab";
+      "alt+right" = "next_tab";
+      "alt+shift+left" = "move_tab_backward";
+      "alt+shift+right" = "next_tab_forward";
+      "ctrl+]" = "launch --cwd=current --location=vsplit";
+      "ctrl+[" = "launch --cwd=current --location=hsplit";
+      "shift+left" = "neighboring_window left";
+      "shift+right" = "neighboring_window right";
+      "shift+up" = "neighboring_window up";
+      "shift+down" = "neighboring_window down";
+      "ctrl+shift+left" = "move_window left";
+      "ctrl+shift+right" = "move_window right";
+      "ctrl+shift+up" = "move_window up";
+      "ctrl+shift+down" = "move_window down";
+      "ctrl+plus" = "change_font_size all +1.0";
+      "ctrl+equal" = "change_font_size all 10.0";
+      "ctrl+minus" = "change_font_size all -1.0";
+      "ctrl+s" = "send_text all \\x17";
+      "ctrl+g" = "send_key alt+d";
+    };
+
+    extraConfig = ''
+      # Scroll mode: bare arrows scroll, escape exits
+      keyboard_mode scroll
+          up         scroll_line_up
+          down       scroll_line_down
+          page_up    scroll_page_up
+          page_down  scroll_page_down
+          k          scroll_line_up
+          j          scroll_line_down
+          u          scroll_half_page_up
+          l          scroll_half_page_down
+          g          scroll_to_top
+          shift+g    scroll_to_bottom
+          escape     pop_keyboard_mode
+          q          pop_keyboard_mode
+          ctrl+c     pop_keyboard_mode
+      end_keyboard_mode
+    '';
+  };
+}
