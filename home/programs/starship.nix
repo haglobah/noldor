b0a26594680ca1eb07a2b600f2c8851e417fc317
Beat Hagenlocher <bah@posteo.de>
2026-06-18 17:44:05 +0200

    Make starship prompt more compact

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -1,4 +1,56 @@
 { config, ... }:
+let
+  compactToolchains = [
+    "bun"
+    "c"
+    "cmake"
+    "cobol"
+    "crystal"
+    "daml"
+    "dart"
+    "deno"
+    "dotnet"
+    "elixir"
+    "elm"
+    "erlang"
+    "fennel"
+    "golang"
+    "gradle"
+    "haskell"
+    "haxe"
+    "helm"
+    "java"
+    "julia"
+    "kotlin"
+    "lua"
+    "nim"
+    "nodejs"
+    "ocaml"
+    "opa"
+    "perl"
+    "php"
+    "pulumi"
+    "purescript"
+    "python"
+    "quarto"
+    "raku"
+    "red"
+    "rlang"
+    "ruby"
+    "rust"
+    "scala"
+    "solidity"
+    "swift"
+    "terraform"
+    "typst"
+    "vagrant"
+    "vlang"
+    "zig"
+  ];
+  compact = {
+    format = "[$symbol$version ]($style)";
+  };
+in
 {
   programs.starship = {
     enable = true;
@@ -28,6 +80,16 @@
           }
         ];
       };
-    };
+
+      nix_shell = {
+        format = "[$symbol$state ]($style)";
+      };
+    }
+    // builtins.listToAttrs (
+      map (n: {
+        name = n;
+        value = compact;
+      }) compactToolchains
+    );
   };
 }
